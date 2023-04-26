492e7b66964e24405e539342c93d0e5117c3b8c4
rohmedina <ro.medina.gatica@gmail.com>
2023-04-26 14:41:24 -0400

    Se suben ultimos cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
 var navbarButton = document.getElementById('navButton')
 var elemento = document.querySelector(".navbar")
 
-
+// Modifica CSS al hacer click del menu
 navbarButton.addEventListener("click", function() {
   if (navbarButton.classList.contains("collapsed")) {
    elemento.style.backdropFilter = "none"
-
   } else {
-     elemento.style.right = "0"
-   elemento.style.position = "fixed"
-   elemento.style.backdropFilter = "blur(10px)"
+    elemento.style.right = "0"
+    elemento.style.position = "fixed"
+    elemento.style.backdropFilter = "blur(10px)"
   }
 });
 
+// Aplica CSS modificado al menu cuando se hace scroll en la landing
 window.addEventListener("scroll", function() {
   var alturaPantalla = window.innerHeight;
   var posicionElemento = elemento.offsetTop;
@@ -25,3 +25,10 @@ window.addEventListener("scroll", function() {
     elemento.classList.remove("navbar-mod");
   }
 });
+
+
+// aplica funcionalidad al tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
